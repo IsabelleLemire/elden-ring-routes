7d704ff2119dd02e930aeee9f4319ed08187f8f8
isabelle_lemire_web <isabellelemire36@gmail.com>
2023-09-11 19:23:35 -0400

    recommence à zero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { Routes, Route } from 'react-router-dom';
-import Creatures from './creatures/components/Creatures';
+
 import Creature from './creatures/components/Creature';
 
 const queryClient = new QueryClient();
@@ -9,10 +8,7 @@ function App() {
 
   return (
       <QueryClientProvider client={queryClient}> 
-        <Routes>
-          <Route path="/" element={<Creatures />} />
-          <Route path='/creature/:name' element={<Creature />} />
-        </Routes>
+        <Creature />
       </QueryClientProvider>
   )
 
--- src/creatures/components/Creature.jsx
+++ src/creatures/components/Creature.jsx
@@ -1,30 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
 import CreatureService from "../service/CreatureService";
 
 const creatureService = new CreatureService();
 
 const Creature = () => {
-    const params = useParams();
+
+    const { isError, isLoading, error, data} = useQuery({
+        queryKey: ["creature"],
+        queryFn: () => creatureService.getAllCreature(),
+    })
+
+    if (isLoading || isError) return <div>Loading...</div>
+    if (isError) return <div>{error.message}</div>
     
-    const { isError, isLoading, error, data} = useQuery(['creature', params.name], () => creatureService.getCreatureById(params.name));
-
-	return (
-		<Container fluid className='min-vh-100 d-grid'>
-			<FetchState isLoading={isLoading} isError={isError} error={error}>
-				<Container>
-					{data && (
-						<div className='d-flex flex-column gap-5'>
-							<CreatureExcerpt creature={data} />
-							<PokemonAbilities creature={data} />
-							<PokemonMoves creature={data} />
-						</div>
-					)}
-				</Container>
-			</FetchState>
-		</Container>
-	)
-
 
     return (
         <div>{data && data.map(creature => {
